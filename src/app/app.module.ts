0fb370cc994a2b91480e594aae551abce1c15cfd
AnndyGrs <andreagarcia_sanchez@hotmail.com>
2018-10-29 11:56:18 -0600

    Agregando modulo de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HereMapsModule } from 'ng2-heremaps';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HereMapsModule.forRoot({
+      apiKey: 'DvtuiM4qevVLHBhosxKs8Q',
+      appId: 'y3MJbr4gRAowxWGxE3cw',
+      apiVersion: '3.0',
+      libraries: ['core', 'service']
+    }),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
